61ebb6ac90967194d95ea71ad6f4549d6d7f74fb
Marc <RhinoDevel@users.noreply.github.com>
2023-01-08 20:08:52 +0100

    Forward FritzBox data via MQTT.

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
 
+// *****************************************************************************
+// *** Retrieve data from AVM FritzBox via TR-064 and forward it to an MQTT  ***
+// *** server.                                                               ***
+// *****************************************************************************
+
 // 2023jan08, Marcel Timm, RhinoDevel
 
-const fb = require('./nodejs-fritzbox/nodejs-fritzbox.js');
+const conf = {
+        fritzBoxUsername: '********',
+        fritzBoxPassword: '********',
+        mqttHostname: '********',
+        mqttTopic: '********'
+    },
 
-fb.exec((o) => console.log(JSON.stringify(o)), '********', '********');
+    fb = require('./nodejs-fritzbox/nodejs-fritzbox.js'),
+    mqtt = require('mqtt'),
+
+    /** To be called on retrieval of data from FritzBox.
+     */
+    onReceived = function(o)
+    {
+        if(o !== null)
+        {
+            let c = mqtt.connect(
+                        'mqtt://' + conf.mqttHostname,
+                        {
+                            connectTimeout: 5000 // ms
+                        });
+
+            c.on(
+                'connect',
+                () => 
+                {
+                    c.publish(conf.mqttTopic, JSON.stringify(o));
+                    c.end();
+                });
+
+            c.on(
+                'error',
+                () => 
+                {
+                    console.log('Error: Some MQTT-related error happened!');
+                    c.end();
+                });
+        }
+    };
+
+fb.exec(onReceived, conf.fritzBoxUsername, conf.fritzBoxPassword);
